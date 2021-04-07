4ec7f8c11582c584c7c5106b9fb527f7b19c14e6
Elias Landsman <elandsma@unca.edu>
2021-04-07 11:59:28 -0400

    changes

--- port-scan.py
+++ port-scan.py
@@ -8,7 +8,7 @@ CSCI373 Networking Lab 2
 import socket
 import os
 
-host = "montreat.cs.unca.edu"
+host = "152.18.69.107"
 
 def main():
     ret_val = os.system("ping -c 1 -W 1 {0} > /dev/null".format(host))
@@ -17,13 +17,13 @@ def main():
     else:
         print(f"Could not ping {format(host)}")
         return
-    ports = range(1,1023)
+    ports = range(1,5555)
     for port in ports:
         try:
             print(f"Trying to connect to {format(host)} on port {format(port)}")
-            sock = socket.create_connection((host, port), timeout=1)
+            sock = socket.create_connection((host, port), timeout=.02)
             #if we get here, TCP connection made, therefore port is open.
-            print(f"\tConnected to port {format(port)}")
+            print(f"\t***********Connected to port {format(port)}")
             sock.close()
         except socket.timeout as e:
             print(f"\tTimeout on port {format(port)}: {e}.")
@@ -31,6 +31,6 @@ def main():
             print(f"\tConnection refused on port {format(port)}: {e}")
         except OSError as e:
             print(f"\tOSError on port {format(port)}: {e}")
-
+    print("done")
 if __name__=='__main__':
     main()
